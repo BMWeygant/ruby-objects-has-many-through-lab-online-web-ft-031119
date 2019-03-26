012ba2db522cf5835b977b7847f3521e54280343
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-26 14:25:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
 attr_accessor :name, :artist, :genre
 
-def initialize(name, artist, genre)
+def initialize(name, artist=nil, genre=nil)
   @name = name
   @artist = artist
   @genre = genre
